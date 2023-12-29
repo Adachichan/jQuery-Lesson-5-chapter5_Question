682871e66bedb2ce7338045c62988994a8fb84c0
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-12-29 12:45:21 +0000

    [ans2] #3 Chapter5_Question_Answer Chapter5の確認問題_正解

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+// javascriptでは、引数の中に関数を入れることができる。
+// slideDown()の引数にfunctionを書き加えることで、スライドダウンした後にfunctionを実行することができるようになる。
+// よってslideDownの中のfunctionに、cssとslideUpの記述を書くことにより、要件を満たすことができる。
 $(function(){
-  $('.box1').slideDown(2000);
-
-  $('.box1').css({
-    'background-color':'#0000FF',
-    'height':'100px'
+  $('.box1').slideDown(2000,function(){
+    $('.box1').css({
+      'background-color':'#0000FF',
+      'width':'200px',
+      'height':'100px'
+    }).slideUp(2000);
   });
-
-  $('.box1').slideUp(2000);
 });
