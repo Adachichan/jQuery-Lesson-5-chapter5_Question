352df98c64d05b436c860ec513793e3c6286dfd2
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-12-29 12:30:14 +0000

    [ans1] #2 Chapter5_Question_Adachi_answer Chapter5の確認問題_足立の解答 ※赤色の正方形を青色の長方形に変える動作を実現できない・・・

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 $(function(){
-  $('.box1').hide(5000);
+  $('.box1').slideDown(2000);
+
+  $('.box1').css({
+    'background-color':'#0000FF',
+    'height':'100px'
+  });
+
+  $('.box1').slideUp(2000);
 });
